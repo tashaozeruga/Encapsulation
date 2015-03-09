7539c41677181e96607c2574e26997101400021e
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 10:47:38 +0200

    This is encapsulation examples project

---
+++ Инкапсуляция/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] logins = { "admin", "root", "vasya" };
+            string[] passwords = { "qwerty", "1234", "1111" };
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+           /* if (login == logins[0] || login == logins[1] || login == logins[2])
+            {
+                Console.WriteLine("Login matches");
+            }
+            else 
+            { 
+                Console.WriteLine("Wrong login");
+            }
+            */
+
+            bool auth = false;
+            for (int i = 0; i < logins.Length; i++)
+            {
+                if (logins[i] == login)
+                {
+                    Console.WriteLine("Login matches");
+                    auth = true;
+                }
+            }
+            if (!auth) Console.WriteLine("Wrong login");
+
+        }
+    }
+}
